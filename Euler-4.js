fbd36fc74b84e3e3159f2d6e317215bf8daf796d
VadimEv <vadim.mayorov@gmail.com>
2020-04-11 01:28:20 +0300

    Update Euler-4.js
    futher improvement, now it's safe up to 5 digit: next: more precise upper bound to start searching

--- Euler-4.js
+++ Euler-4.js
@@ -16,24 +16,25 @@ function constructNum (num) {
   return parseInt(str.padEnd(num, str), 10)
 }
 /* not nearly the optimal way, revisiting it later */
-/* Safe to assume that palindromes lie in upper halves */
+/* Safe to assume that palindromes lie in upper half and due to symmetry we need only half of those */
 function matrix (num) {
-  var arr = []
+  var max = 0
   // Creates all lines:
-  for (var i = Math.floor(num / 2); i < num + 1; i++) {
-    for (var j = Math.floor(num / 2); j < num + 1; j++) {
+  for (var i = Math.floor(num * 0.8); i < num + 1; i++) {
+    for (var j = i; j < num + 1; j++) {
       // Initializes:
-      if (isPalindrome(i * j)) { arr.push(i * j) }
+      if (isPalindrome(i * j) && (max < i * j)) {
+        max = i * j
+      }
     }
   }
-  return arr
+  return max
 }
 function largestPalindromeProduct (num) {
   // construct the dimension
   const dim = constructNum(num)
   // remove all zero and non palidrome numbers
-  const pals = matrix(dim)
-  return Math.max.apply(Math, pals)
+  return matrix(dim)
 }
 
 largestPalindromeProduct(2)
